26e5810d64bf848781ef9fdb56941685ee391f6f
joelschum@gmail.com <joelschum@gmail.com>
2015-04-25 18:54:23 +0200

    Spaceship camera now starts at player

--- spaceship.lua
+++ spaceship.lua
@@ -24,6 +24,7 @@ do
                     spaceship.astronaut = {
                         position = {tileToWorld(unpack(spaceship.map.spawn))}
                     }
+                    camera.targetX, camera.targetY = unpack(spaceship.astronaut.position)
 
                     event.peer:send("INITD") -- initialization done
                 elseif type == "PLPOS" then
